6d17e949ef3e18aa61e58e195a854aeb69acda12
Nicolas Bia <n.bia@outlook.fr>
2021-11-18 23:30:12 +0100

    exo1

---
+++ script1.js
@@ -0,0 +1,13 @@
+function hello() {
+  console.log("Bonjour monde");
+}
+
+
+
+function helloname() {
+  let username = prompt("Yo ! Quel est ton nom l'ami(e) ?");
+  console.log("Salutations, " + username);
+}
+
+hello();
+helloname();
